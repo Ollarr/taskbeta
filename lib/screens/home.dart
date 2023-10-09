3569ba15c36b91e05d13043af41ffdd63af4153b
Oluwasegun <oluyedis@gmail.com>
2023-10-09 12:25:50 +0100

    Worked on the home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:taskbeta/constants/colors.dart';
 
 class Home extends StatelessWidget {
-
-  const Home ({super.key});
+  const Home({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return const Scaffold();
-
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: tbBGColor,
+        title: const Row(
+          children: [
+            Icon(
+              Icons.menu,
+              color: tbBlack,
+              size: 30,
+            )
+          ],
+        ),
+      ),
+      body: Container(child: const Text("This is the home screen")),
+    );
   }
 }
